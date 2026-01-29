07a2c593bd47184bb2b5ff376c44495cb137c477
PapaBear <nishatislam84@gmail.com>
2026-01-29 09:52:57 +0600

    added a demo landing page and app route

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def landing_page():
+    return render_template('landingPage.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5500)
