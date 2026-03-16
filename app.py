38e2035d68806983708bbb5597459fc17432880a
Asif Amir Noor <asifcyberg@gmail.com>
2026-03-16 09:25:57 +0600

    moved credentials to .env

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ load_dotenv()
 app.secret_key = os.getenv("APP.SECRET") 
 
 # --- Configure Flask-Mail ---
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'asifcyberg@gmail.com'
-app.config['MAIL_PASSWORD'] = 'isuvlgwpizgotmll'  # Use 16-character Google App Password
-app.config['MAIL_DEFAULT_SENDER'] = 'asifcyberg@gmail.com'
+app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
+app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT"))
+app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS") == "True"
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")  # Use 16-character Google App Password
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_DEFAULT_SENDER")
 
 mail = Mail(app)
 
